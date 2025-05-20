af611e4bccfb7a6b0f3a25e687078fc33b657b53
Josh <JoshLynch2001@gmail.com>
2025-05-20 02:32:34 -0300

    i guess im tired

---
+++ src/Navbar/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <div className="navbar">
+        <div className="logo">My Logo</div>
+        <div className="menu">
+            <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            </ul>
+        </div>
+        </div>
+    )
+};
+
+export default Nav;
