a06ad8c510dd6409ebf005261cb0b32e56c41da6
Josh <JoshLynch2001@gmail.com>
2025-06-26 23:54:58 -0300

    My new Portfolio base

---
+++ src/layouts/default.tsx
@@ -0,0 +1,23 @@
+import { Link } from "@heroui/link";
+
+import { Navbar } from "@/components/navbar";
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex flex-col h-screen">
+      <Navbar />
+      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+        {children}
+      </main>
+      <footer className="w-full flex items-center justify-center py-3">
+        <Link isExternal >
+          <span className="text-default-600">Joshua Lynch 2025</span>
+        </Link>
+      </footer>
+    </div>
+  );
+}
